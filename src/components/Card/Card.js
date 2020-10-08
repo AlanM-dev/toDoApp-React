8082a0078a2715d7e923961f3eb5e8ccdeede949
Alan Marcinkiewicz <marcinkiewicz.alan@gmail.com>
2020-10-08 17:57:58 +0200

    Add empty Card.js

---
+++ src/components/Card/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
