de8ee8408e4c8d9b08d0536d5dde772cfc2d3480
Alan Marcinkiewicz <marcinkiewicz.alan@gmail.com>
2020-10-09 09:25:56 +0200

    Add Card, Icon // Module 15.5 -> DONE

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
-import {settings} from '../../data/dataStore';
-import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+
+
+
+class Card extends React.Component {
+
+    static PropTypes = {
+        title: PropTypes.string,
+    }
+    
+    render() {
+        return (
+            <div className={styles.component}>
+                <h3>{this.props.title}</h3>
+            </div>
+        )
+    }
+}
+
+export default Card;
