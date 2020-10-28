37079a3b49c32632d2fa5b4c1afca2b3f0fd5368
Alan Marcinkiewicz <marcinkiewicz.alan@gmail.com>
2020-10-28 15:18:22 +0100

    Add ColumnContainer - problem / no cards in columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-// import Card from '../Card/Card.js';
+import Card from '../Card/Card.js';
 // import Creator from '../Creator/Creator.js';
 // import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
@@ -16,19 +16,20 @@ class Column extends React.Component {
   }
 
   render() {
-    const {title, icon} = this.props;
+    const {title, icon, cards} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}><span className={styles.icon}>
           <Icon name={icon} />
         </span>{title}</h3>
-        {/* <div className={styles.cards}>
-          {this.state.cards.map(({key, ...cardProps}) => (
-            <Card key={key} {...cardProps} />))}
+        <div className={styles.cards}>
+          {cards.map(cardsData => (
+            <Card key={cardsData.id} {...cardsData} /> 
+          ))}
         </div>
-        <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div> */}
+        {/* <div className={styles.creator}>
+           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+         </div> */}
       </section>
     );
   }
