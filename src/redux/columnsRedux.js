9488f83f7c240f16245d134b19fcae36d8052838
Alan Marcinkiewicz <marcinkiewicz.alan@gmail.com>
2020-10-30 10:08:11 +0100

    Fix columnsRedux

--- src/redux/columnsRedux.js
+++ src/redux/columnsRedux.js
@@ -1,6 +1,6 @@
 import shortid from 'shortid';
 
-// selectors
+// selectors  //// {columns}= wyciągnij z tego obiektu, w którym był stan - tylko tę jedną właściwość
 export const getColumnsForList = ({columns}, listId) => columns.filter(column => column.listId == listId);
 
 // action name creator
@@ -14,11 +14,11 @@ export const ADD_COLUMN = createActionName('ADD_COLUMN');
 export const createActionAddColumn = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_COLUMN });
 
 // reducer
-export default function reducer(state = [], action = {}) {
+export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
     case ADD_COLUMN:
-      return [...state, action.payload];
+      return [...statePart, action.payload];
     default:
-      return state;
+      return statePart;
   }
 }
